16e3121356bbc00be8805e11ef34b6d8e8496673
Veselin <veselin.dok@gmail.com>
2026-01-24 13:46:26 +0100

    Added type to data

--- app/sstable/data.go
+++ app/sstable/data.go
@@ -9,6 +9,7 @@ import (
 type Entry struct {
 	Timestamp int64
 	Tombstone byte
+	Type      byte
 	Key       []byte
 	Value     []byte
 }
@@ -17,43 +18,44 @@ func (e *Entry) Serialize() []byte {
 	keySize := uint64(len(e.Key))
 	valueSize := uint64(len(e.Value))
 
-	buf := make([]byte, 0)
+	payload := make([]byte, 0)
 
-	tmp := make([]byte, 8)
-	binary.LittleEndian.PutUint64(tmp, uint64(e.Timestamp))
-	buf = append(buf, tmp...)
+	tmp8 := make([]byte, 8)
 
-	buf = append(buf, e.Tombstone)
-
-	binary.LittleEndian.PutUint64(tmp, keySize)
-	buf = append(buf, tmp...)
-
-	binary.LittleEndian.PutUint64(tmp, valueSize)
-	buf = append(buf, tmp...)
-
-	buf = append(buf, e.Key...)
-	buf = append(buf, e.Value...)
-
-	crc := crc32.ChecksumIEEE(buf)
+	binary.LittleEndian.PutUint64(tmp8, uint64(e.Timestamp))
+	payload = append(payload, tmp8...)
+	payload = append(payload, e.Tombstone)
+	binary.LittleEndian.PutUint64(tmp8, keySize)
+	payload = append(payload, tmp8...)
+	binary.LittleEndian.PutUint64(tmp8, valueSize)
+	payload = append(payload, tmp8...)
+	payload = append(payload, e.Type)
+	payload = append(payload, e.Key...)
+	payload = append(payload, e.Value...)
+	crc := crc32.ChecksumIEEE(payload)
 	crcBytes := make([]byte, 4)
 	binary.LittleEndian.PutUint32(crcBytes, crc)
 
-	return append(crcBytes, buf...)
+	return append(crcBytes, payload...)
 }
 
 func DeserializeEntry(file *os.File) (*Entry, error) {
-	header := make([]byte, 4+8+1+8+8)
+	header := make([]byte, 4+8+1+8+8+1)
+
 	_, err := file.Read(header)
 	if err != nil {
 		return nil, err
 	}
 
 	e := &Entry{}
+
 	e.Timestamp = int64(binary.LittleEndian.Uint64(header[4:12]))
 	e.Tombstone = header[12]
 
 	keySize := binary.LittleEndian.Uint64(header[13:21])
 	valueSize := binary.LittleEndian.Uint64(header[21:29])
+
+	e.Type = header[29]
 
 	e.Key = make([]byte, keySize)
 	_, err = file.Read(e.Key)

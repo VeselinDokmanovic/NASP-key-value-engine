a95b32fa0078f3cc76bcfb324230988004b547b7
Veselin <veselin.dok@gmail.com>
2026-01-24 14:12:17 +0100

    Implemented index, added constants file

---
+++ app/sstable/constants.go
@@ -0,0 +1,6 @@
+package sstable
+
+const (
+	INDEX_KEY_SIZE   = 32
+	INDEX_ENTRY_SIZE = INDEX_KEY_SIZE + 8
+)
---
+++ app/sstable/index.go
@@ -0,0 +1,48 @@
+package sstable
+
+import (
+	"bytes"
+	"encoding/binary"
+	"os"
+)
+
+type IndexEntry struct {
+	Key    []byte
+	Offset uint64
+}
+
+func SerializeIndexEntry(key []byte, offset uint64) []byte {
+	buf := make([]byte, INDEX_ENTRY_SIZE)
+
+	copy(buf[:INDEX_KEY_SIZE], key)
+
+	binary.LittleEndian.PutUint64(
+		buf[INDEX_KEY_SIZE:INDEX_KEY_SIZE+8],
+		offset,
+	)
+
+	return buf
+}
+
+func ReadIndexEntry(file *os.File) (*IndexEntry, error) {
+	buf := make([]byte, INDEX_ENTRY_SIZE)
+
+	_, err := file.Read(buf)
+	if err != nil {
+		return nil, err
+	}
+
+	rawKey := buf[:INDEX_KEY_SIZE]
+
+	// skidamo padding nule
+	key := bytes.TrimRight(rawKey, "\x00")
+
+	offset := binary.LittleEndian.Uint64(
+		buf[INDEX_KEY_SIZE : INDEX_KEY_SIZE+8],
+	)
+
+	return &IndexEntry{
+		Key:    key,
+		Offset: offset,
+	}, nil
+}
